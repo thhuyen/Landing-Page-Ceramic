8019e96dfa174d7d67c9fbbe2a9d6f57f5dfa3a6
thhuyen <97439051+thhuyen@users.noreply.github.com>
2023-03-27 23:14:31 +0700

    80% products
    product best sellers tabs done
    product hot deals done
    banner done
    add position fixed header
    function render (to render all), setHTML (support render products), findAppear (toggle classList to show or hide element) done
    file render.js (only render)
    file eventHandler.js (only handle event)
    file loadPage.js to render same element like header, footer,...
    class w3-include-html to contain element after loadPage.js returns UI
    file product.css includes css of all products

--- assets/js/home/eventHandler.js
+++ assets/js/home/eventHandler.js
@@ -1,10 +1,10 @@
 const findAppear = (type, name, name_replace) => {
     if (Array.isArray(type)) {
         const list = type.filter(e => e.className.includes(name));
-        list[0].className = list[0].className.replace(name, name_replace)
+        list[0].className = list[0].className.replace(name, name_replace);
     }
     else {
-        type.className = type.className.replace(name, name_replace)
+        type.className = type.className.replace(name, name_replace);
     }
 }
 
@@ -12,8 +12,15 @@ const handleChangeSlide = (dot) => {
     if (dot.className.includes('solid'));
     else {
         findAppear([...$$('.icon-dot')], 'solid', 'regular');
-        findAppear([...$$('.slider-imgs')], 'active', '');
+        findAppear([...$$('.slider-imgs')], 'active_block', '');
         findAppear(dot, 'regular', 'solid');
-        $$('.slider-imgs')[+dot.id.slice(4,5)].classList.add('active');
+        $$('.slider-imgs')[+dot.id.slice(4,5)].classList.add('active_block');
     }
+}
+
+const handleChangeTabs = (button) => {
+    findAppear([...$$('.btn-bestsl')], 'active_color', '');    
+    button.classList.add('active_color');
+    findAppear([...$$('.products-main')], 'active_grid', '');
+    $$('.products-main')[+button.className.slice(26, 27)].classList.add('active_grid');
 }
