1e339a5da3d199dfb7238698ccce92a7030108fc
thhuyen <97439051+thhuyen@users.noreply.github.com>
2023-04-04 11:21:53 +0700

    add fake api and fix some UI
    After pulling, please run a code: npm i, then run npm start at product_json folder
    Download font themify icon to see all icons on web.

--- assets/js/detail/render.js
+++ assets/js/detail/render.js
@@ -1,44 +1,26 @@
-const bestSellerProduct = [
-    {
-        name: 'Bulb Vase',
-        price: 21,
-        discount_percent: null,
-        img: '//cdn.shopify.com/s/files/1/0660/1985/2535/products/5.1.jpg?v=1662001020'
-    },
-    {
-        name: 'Bud Vase',
-        price: 20,
-        discount_percent: null,
-        img: '//cdn.shopify.com/s/files/1/0660/1985/2535/products/6.1.jpg?v=1662001016'
-    },
-    {
-        name: 'Ceramic Planter Pot',
-        price: 18,
-        discount_percent: 12,
-        img: '//cdn.shopify.com/s/files/1/0660/1985/2535/products/4.1.jpg?v=1662001013'
-    },
-    {
-        name: 'Ceramic Teapot',
-        price: 14,
-        discount_percent: null,
-        img: '//cdn.shopify.com/s/files/1/0660/1985/2535/products/3.1.jpg?v=1662001008'
-    },
-]
+fetch(apiProducts) 
+    .then (response => response.json())
+    .then (products => {
+        const bestSellerProduct = products.filter(e => e.type === "best seller");
+        // render related products
+        for(let i = 0 ; i < 4; i++) {
+            render($('.products-related'), 'div', 'product_bestsl', null, setHTMLProduct(bestSellerProduct[i]), null, null)
+        }
+    })
+    .catch (error => {throw new Error(error)})
 
 
-// render related products
-for(let i = 0 ; i < 4; i++) {
-    render($('.products-related'), 'div', 'product_bestsl', null, setHTMLProduct(bestSellerProduct[i]), null, null)
-}
-
+// img product 
 render($('#in4_product'), 'img', 'img-slider' ,null, null, null);
 $('.img-slider').src = localStorage.getItem("productImg");
+
+// information product
 render($('#in4_product'), 'div', 'desc_product', null, 
     `<h2 class="name_product">${localStorage.getItem("productName")}</h2>
     ${localStorage.getItem("productDiscount") ? 
-        `<span class="origin_price">${localStorage.getItem("productPrice")} USD</span>
-        <span class="sale_price"> &nbsp; ${localStorage.getItem("productDiscount")} USD</span>` : 
-        `<span class="sale_price"> &nbsp; ${localStorage.getItem("productPrice")} USD</span>` }
+        `<span class="price origin_price discount">${localStorage.getItem("productPrice")} USD</span>
+        <span class="price discount_price"> &nbsp; ${localStorage.getItem("productDiscount")} USD</span>` : 
+        `<span class="price discount_price"> &nbsp; ${localStorage.getItem("productPrice")} USD</span>` }
     <p class="desc_detail">
         Things You Need To Know Using advanced edging technology and high-quality materials, our premium sunglasses have a luxurious look and feel with subtly curved lenses. These striking, sophisticated sunglasses offer timeless design updated with shiny gold metal accents at the temples for just a bit of bling. The wide frame...
     </p>
