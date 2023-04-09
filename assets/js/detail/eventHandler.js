63dcc714716d2e37de596c60fddbebde76b37772
thhuyen <97439051+thhuyen@users.noreply.github.com>
2023-04-09 11:22:49 +0700

    Update eventHandler.js

--- assets/js/detail/eventHandler.js
+++ assets/js/detail/eventHandler.js
@@ -42,20 +42,7 @@ const createIdGenerator = () => {
   const S4 = () => {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   };
-  return (
-    S4() +
-    S4() +
-    "-" +
-    S4() +
-    "-" +
-    S4() +
-    "-" +
-    S4() +
-    "-" +
-    S4() +
-    S4() +
-    S4()
-  );
+  return (S4() + S4() + "-" + S4() + "-" + S4() +  "-" + S4() + "-" + S4() + S4() + S4());
 };
 
 // set date for comments
@@ -213,8 +200,8 @@ const handleComment = button => {
     }
 
     updateCmtArr(newComment);
-  }
-};
+}
+
 
 // handle like comment
 const handleLike = (icon_heart) => {
